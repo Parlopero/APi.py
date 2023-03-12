3150338c5f13fc2386d3c9f24dc4b99a907ea997
Parlopero <127315562+Parlopero@users.noreply.github.com>
2023-03-12 17:58:42 -0300

    Update Api_receita.py

--- Api_receita.py
+++ Api_receita.py
@@ -2,35 +2,36 @@ import requests
 import json
 
 
-
 def consulta_cnpj(cnpj):
+    url = f'https://receitaws.com.br/v1/cnpj/{cnpj}'
+    querystring = {"token": "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX", "cnpj": "06990590000123", "plugin": "RF"}
+    response = requests.request('GET', url, params=querystring)
 
-     url = f'https://receitaws.com.br/v1/cnpj/{cnpj}'
-     querystring = {"token": "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX", "cnpj": "06990590000123", "plugin": "RF"}
-     response = requests.request('GET', url, params=querystring)
+    resp = json.loads(response.text)
 
-     resp = json.loads(response.text)
+    print(resp['nome'], resp['logradouro'], resp['numero'], resp['complemento'], resp['bairro']), resp['municipio'], \
+    resp['uf'], resp['cep'], resp['telefone'], resp['email']
 
-     print(resp['nome'] ,resp['logradouro'], resp['numero'], resp['complemento'], resp['bairro'])
-
-     return resp['nome'] ,resp['logradouro'], resp['numero'], resp['complemento'], resp['bairro']
+    return resp['nome'], resp['logradouro'], resp['numero'], resp['complemento'], resp['bairro'], resp['municipio'], \
+    resp['uf'], resp['cep'], resp['telefone'], resp['email']
 
 
 print('####################')
-print('### Consulta CEP ###')
+print('### Consulta CNPJ ###')
 print('####################')
 print()
 
 cnpj = input('Digite o CNPJ para a consulta: ')
 
-
 if len(cnpj) != 14:
-		print('Quantidade de dígitos inválida!')
-		exit()
+    print('Quantidade de dígitos inválida!')
+    exit()
 
-
-print('==> CEP ENCONTRADO <==')
-consulta_cnpj(''+cnpj+'')
+print('==> CNPJ ENCONTRADO <==')
+consulta_cnpj('' + cnpj + '')
 
 print('---------------------------------')
-option = int(input('Deseja realizar uma nova consulta ?\n 1. Sim\n 2. Sair\n'))
+print('Essas são as informações encontradas no seu cnpj!')
+
+print('---------------------------------')
+print('saindo...')
