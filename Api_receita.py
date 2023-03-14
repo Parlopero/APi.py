8ea5a655ad6d44e5883c56a2bfbf58ce84689e33
Parlopero <127315562+Parlopero@users.noreply.github.com>
2023-03-14 01:20:33 -0300

    Update Api_receita.py

--- Api_receita.py
+++ Api_receita.py
@@ -2,19 +2,18 @@ import requests
 import json
 
 
-def consulta_cnpj(cnpj):
+def consulta_cnpj(cnpj) -> object:
     url = f'https://receitaws.com.br/v1/cnpj/{cnpj}'
     querystring = {"token": "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX", "cnpj": "06990590000123", "plugin": "RF"}
-    response = requests.request('GET', url, params=querystring)
+    response = requests.request('GET', url,params=querystring)
 
     resp = json.loads(response.text)
 
     print(resp['nome'], resp['logradouro'], resp['numero'], resp['complemento'], resp['bairro']), resp['municipio'], \
-    resp['uf'], resp['cep'], resp['telefone'], resp['email']
+        resp['uf'], resp['cep'], resp['telefone'], resp['email']
 
     return resp['nome'], resp['logradouro'], resp['numero'], resp['complemento'], resp['bairro'], resp['municipio'], \
-    resp['uf'], resp['cep'], resp['telefone'], resp['email']
-
+        resp['uf'], resp['cep'], resp['telefone'], resp['email']
 
 print('####################')
 print('### Consulta CNPJ ###')
@@ -23,13 +22,19 @@ print()
 
 cnpj = input('Digite o CNPJ para a consulta: ')
 
-if len(cnpj) != 14:
+string = "" + cnpj + ""
+characters = ".-/"
+
+string = ''.join(x for x in string if x not in characters)
+
+if len(string) != 14:
     print('Quantidade de dígitos inválida!')
     exit()
-
-print('==> CNPJ ENCONTRADO <==')
-consulta_cnpj('' + cnpj + '')
-
+try:
+    print('==> CNPJ ENCONTRADO <==')
+    consulta_cnpj('' +string+ '')
+except:
+  print('{}: CNPJ inválido.'.format(cnpj))
 print('---------------------------------')
 print('Essas são as informações encontradas no seu cnpj!')
 
